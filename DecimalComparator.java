b296a099b122c6b0ee4ea38d469758d179c3b38e
Moizuddin Qureshi <30798716+imoizuddin@users.noreply.github.com>
2020-01-21 22:10:05 +0530

    Coding Excercises for Section 4
    Code Solutions for the following topics : Expressions, Statements, Code blocks and Methods

---
+++ DecimalComparator.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
+        int a = (int) (x*1000);
+        int b = (int) (y*1000);
+
+        if (a==b){
+            return true;
+        }
+        else return false;
+    }
+}
